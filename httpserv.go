9afd85420d1d2cb952ea11e05874671426969aeb
Narong Sungkhamalai <narong.skml@gmail.com>
2016-08-22 15:43:44 +0700

    add sample http server with go

--- httpserv.go
+++ httpserv.go
@@ -1,8 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
-func    main(){
-    
-    
+func main() {
+
+	http.HandleFunc("/", get)
+	http.ListenAndServe(":8080", nil)
+}
+
+func get(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello http!")
 }
